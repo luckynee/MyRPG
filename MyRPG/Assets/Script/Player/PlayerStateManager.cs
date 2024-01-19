f519e3dc9f0cee6a0d3b3de45cf5586f9f248525
luckynee <81769081+luckynee@users.noreply.github.com>
2024-01-19 21:50:59 +0700

    Lucky-Update-Movement

---
+++ MyRPG/Assets/Script/Player/PlayerStateManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerStateManager : MonoBehaviour 
+{
+    private PlayerMovement playerMovement;
+
+    private enum PlayerState
+    {
+        Idle,
+        Walking,
+        Sprinting,
+        OnAir,
+        Dash,
+    }
+
+    private PlayerState playerState;
+
+    private void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void Start()
+    {
+        playerMovement.OnWalking += PlayerMovement_OnWalking;
+        playerMovement.OnIdle += PlayerMovement_OnIdle;
+        playerMovement.OnAir += PlayerMovement_OnAir;
+    }
+
+    private void PlayerMovement_OnAir(object sender, System.EventArgs e)
+    {
+        playerState = PlayerState.OnAir;
+    }
+
+    private void PlayerMovement_OnIdle(object sender, System.EventArgs e)
+    {
+        playerState = PlayerState.Idle;
+    }
+
+    private void PlayerMovement_OnWalking(object sender, System.EventArgs e)
+    {
+        playerState = PlayerState.Walking;
+    }
+
+    private void Update()
+    {
+        switch (playerState)
+        {
+            case PlayerState.Idle:
+                break;
+            case PlayerState.Walking:
+                break;
+            case PlayerState.Sprinting:
+                break;
+            case PlayerState.OnAir:
+                break;
+            case PlayerState.Dash:
+                break;
+        }
+
+        Debug.Log(playerState);
+    }
+
+   
+
+    
+
+}
+
+
---
+++ MyRPG/Assets/Script/StateMachine/PlayerIdleState.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIdleState : PlayerBaseState
+{
+    public PlayerIdleState(PlayerStateMachine currentContext, PlayerStateFactory factory) : base(currentContext, factory) { }
+
+    public override void CheckSwitchState()
+    {
+        
+    }
+
+    public override void EnterState()
+    {
+       
+    }
+
+    public override void ExitState()
+    {
+       
+    }
+
+    public override void InitializeSubState()
+    {
+       
+    }
+
+    public override void UpdateState()
+    {
+        CheckSwitchState();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
